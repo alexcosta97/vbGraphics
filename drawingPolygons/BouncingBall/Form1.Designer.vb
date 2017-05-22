d832cbb99d18d4f4128cd8482cb888437b083e82
Alex Costa <alex.callum.c@gmail.com>
2017-05-22 23:09:09 +0100

    Finished all tasks

---
+++ drawingPolygons/BouncingBall/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pbxDisplay = New System.Windows.Forms.PictureBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pbxDisplay, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbxDisplay
+        '
+        Me.pbxDisplay.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.pbxDisplay.Location = New System.Drawing.Point(20, 10)
+        Me.pbxDisplay.Margin = New System.Windows.Forms.Padding(2)
+        Me.pbxDisplay.Name = "pbxDisplay"
+        Me.pbxDisplay.Size = New System.Drawing.Size(388, 248)
+        Me.pbxDisplay.TabIndex = 0
+        Me.pbxDisplay.TabStop = False
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(56, 274)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(84, 22)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(169, 274)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(78, 22)
+        Me.btnStop.TabIndex = 2
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(302, 274)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(100, 22)
+        Me.btnQuit.TabIndex = 3
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(438, 318)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.pbxDisplay)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bouncing Ball"
+        CType(Me.pbxDisplay, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private WithEvents pbxDisplay As PictureBox
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ drawingPolygons/BouncingBall/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+    Dim rand As New Random
+    Dim x As Integer = 200
+    Dim y As Integer = 50
+    Dim xBall2 As Integer = 0
+    Dim yBall2 As Integer = 0
+    Dim xmove As Integer = 10
+    Dim ymove As Integer = 10
+    Dim x2move As Integer = 10
+    Dim y2move As Integer = 10
+    Dim size As Integer = 30
+    Dim time As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        x = rand.Next(300) + 27
+        y = rand.Next(200) + 12
+        xBall2 = rand.Next(300) + 27
+        yBall2 = rand.Next(200) + 12
+    End Sub
+
+    Private Sub pbxDisplay_Paint(sender As Object, e As PaintEventArgs) Handles pbxDisplay.Paint
+        Dim g As Graphics = e.Graphics
+        g.FillEllipse(Brushes.Red, x, y, size, size)
+        g.FillEllipse(Brushes.Red, xBall2, yBall2, size, size)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ''Verifies movement against the walls after the new position is set to the draw
+        x += xmove
+        y += ymove
+        If (y + size >= pbxDisplay.Height) Then
+            ymove = -ymove
+        ElseIf (x + size >= pbxDisplay.Width) Then
+            xmove = -xmove
+        ElseIf (x + (size - 10) <= pbxDisplay.Location.X) Then
+            xmove = -xmove
+        ElseIf (y + (size - 15) <= pbxDisplay.Location.Y) Then
+            ymove = -ymove
+        End If
+        Refresh()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        ''Verifies movement against the walls after the new position is set to the draw
+        xBall2 += x2move
+        yBall2 += y2move
+        If (yBall2 + size >= pbxDisplay.Height) Then
+            y2move = -y2move
+        ElseIf (xBall2 + size >= pbxDisplay.Width) Then
+            x2move = -x2move
+        ElseIf (xBall2 + (size - 10) <= pbxDisplay.Location.X) Then
+            x2move = -x2move
+        ElseIf (yBall2 + (size - 15) <= pbxDisplay.Location.Y) Then
+            y2move = -y2move
+        End If
+
+        ''checks if the balls are going against each other or not
+        If (calcDist((x - xBall2), (y - yBall2)) <= size + 3) Then
+            x2move = -x2move
+            y2move = -y2move
+            xmove = -xmove
+            ymove = -ymove
+        End If
+
+        Refresh()
+    End Sub
+
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
+        If (keyData = Keys.Up) Then
+            size += 5
+            Refresh()
+            Return True
+        ElseIf (keyData = Keys.Down) Then
+            size -= 5
+            Refresh()
+            Return True
+        ElseIf (keyData.ToString() = "C") Then
+            time += 1
+            If (time > 10) Then
+                time = 0
+            End If
+            Select Case time
+                Case 0
+                    pbxDisplay.BackColor = Color.Aqua
+                Case 1
+                    pbxDisplay.BackColor = Color.Yellow
+                Case 2
+                    pbxDisplay.BackColor = Color.Violet
+                Case 3
+                    pbxDisplay.BackColor = Color.Blue
+                Case 4
+                    pbxDisplay.BackColor = Color.Green
+                Case 5
+                    pbxDisplay.BackColor = Color.Gold
+                Case 6
+                    pbxDisplay.BackColor = Color.Gray
+                Case 7
+                    pbxDisplay.BackColor = Color.Ivory
+                Case 8
+                    pbxDisplay.BackColor = Color.LemonChiffon
+                Case 9
+                    pbxDisplay.BackColor = Color.Chocolate
+                Case 10
+                    pbxDisplay.BackColor = Color.SlateBlue
+            End Select
+            Return True
+        End If
+    End Function
+
+    Private Function calcDist(a As Integer, b As Integer) As Double
+        Return Math.Sqrt((Math.Pow(a, 2) + Math.Pow(b, 2)))
+    End Function
+
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Application.Exit()
+    End Sub
+End Class
